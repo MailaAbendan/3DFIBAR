1b3aa5998651573912c12f3c339d794186df0889
MailaAbendan <abendan.maila16@gmail.com>
2025-03-08 19:57:09 +0800

    Add files via upload

---
+++ 3DFIBAR/3DFIBAR.bat
@@ -0,0 +1,12 @@
+@echo off
+echo WHAT DO YOU WANT TO GENERATE
+set /p filename=
+echo Alright, I will generate the animation into a 3D
+echo Generating (1/3000)...
+timeout /t 3 >nul
+echo Generating (2/3000)...
+timeout /t 3 >nul
+echo Generating (3000/3000)...
+timeout /t 1 >nul
+echo Generation complete. Your file is VideoTest1.mp4
+echo Enjoy watching!
